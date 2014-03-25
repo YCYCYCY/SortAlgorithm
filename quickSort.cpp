1033340a73f7927a1d292dbd44f85eca651dfaf0
heyuanchuan <heyuanchuan1991@gmail.com>
2014-03-25 09:57:25 +0800

    add shell sort

--- quickSort.cpp
+++ quickSort.cpp
@@ -6,15 +6,15 @@ void quickSort(int *num,int begin,int end)
 	while(i<j)
 	{
 		for(i=i+1;i<end;i++)
-		if{num[i]>compara)
+		if{num[i]>compara}
 		break;
-		for (j;j>=begin;--j)
+    for (j=j-1;j>=begin;--j)
 		if(num[j]>compara)
 		break;
 		if(j>i)
 		swap(num[i],num[j]);
 	}
 	swap(num[begin],num[j]);
-	quickSort(num,begin,i-1);
-	quickSort(num,j+1;end);
+	quickSort(num,begin,j-1);
+	quickSort(num,i;end);
 }
---
+++ shellSort.cpp
@@ -0,0 +1,11 @@
+void shellSort(int *num,int size)
+{
+    int len=size;
+    while(len>1)
+    {
+        len=(len+1)/2;
+        for(int i=0;i<size-len)
+            if(num[i]>num[i+len])
+                swp(num[i],num[i+len]);
+    }
+}
