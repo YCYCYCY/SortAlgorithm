f19f4d718c3d4a6e19d931ec6443833aa922eefe
heyuanchuan <heyuanchuan1991@gmail.com>
2014-11-23 18:04:11 +0800

    modify shell sort

--- shellSort.cpp
+++ shellSort.cpp
@@ -1,11 +1,24 @@
 void shellSort(int *num,int size)
 {
-    int len=size;
-    while(len>1)
+    int offset=size/2;
+    while(offset>0)
     {
-        len=(len+1)/2;
-        for(int i=0;i<size-len;i++)
-            if(num[i]>num[i+len])
-                swp(num[i],num[i+len]);
+        for(int i=offset;i<size;++i)
+        {
+            int compara=num[i];
+            //insert sort
+            #ifdef FUN1
+            for(int j=i-offset;j>=0 && num[j]>compara;j-=offset)
+                num[j+offset]=num[j];
+            num[j+offset]=compara;
+            #endif //FUN1
+            
+            #ifdef FUN2
+            for(int j=i;j>=offset && num[j-offset]>compara;j-=offset)
+                num[j]=num[j-offset];
+            num[j]=compara;
+            #endif //FUN2
+        }
+        offset/=2;
     }
 }
